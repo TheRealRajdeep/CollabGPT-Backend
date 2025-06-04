1b74d69930af20c1fe58d01666cc58e7c961a17c
TheRealRajdeep <rajdeepjadeja2806@gmail.com>
2025-06-04 11:43:56 +0530

    apply cors headers "access-control-allow-origin"

--- index.js
+++ index.js
@@ -36,19 +36,19 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// // Add CORS headers to all responses
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, X-Requested-With"
-//   );
-//   if (req.method === "OPTIONS") {
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
+// Add CORS headers to all responses
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // Create HTTP server
 const server = http.createServer(app);
